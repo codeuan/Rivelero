76b27d0d9bfa84ec69100f4c8de306585f2d04f5
Connor Dean-Pijuan <connor.dean2007@gmail.com>
2026-04-14 18:34:09 +0200

    Create optimiser.py

---
+++ src/optimiser.py
@@ -0,0 +1,6 @@
+#optimiser.py
+#categorise a set of images in order of how optimal they are for the training data.
+#factors considered: mean visibility strength, botanical suitabity, presence of obstacles.
+
+
+
